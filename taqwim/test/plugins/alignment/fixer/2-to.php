c02da954123a0bc43e487cf8d7e7e4968f8b8f35
Abdul Al-Hasany <kahhar87@gmail.com>
2023-07-18 16:37:28 +1000

    fix: add more tests for docblock alignment

---
+++ taqwim/test/plugins/alignment/fixer/2-to.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * This is a test class.
+ */
+class Test
+{
+	/**
+	 * Log info.
+	 *
+	 * @param string $message Message to log
+	 * @return bool True on success, false on failure
+	 */
+    public function logInfo($message)
+    {
+        echo $message;
+        return true;
+    }
+
+   /**
+    * Log error.
+    * @param string $message Message to log
+    */
+    private function logError($message)
+    {
+        echo $message;
+        return true;
+    }
+}
